35fac1ddfc9eb9ce30411d5aaa642772af5fc251
Khoi Nguyen <dangkhoi.kt@gmail.com>
2026-09-21 21:50:06 +0700

    chore: bump version to 1.3.1

--- patch.py
+++ patch.py
@@ -7,7 +7,7 @@ import os
 import sys
 import argparse
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 REPO_URL = "https://github.com/khoinguyen88kt/chrome-macpro-gpu-patch"
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
